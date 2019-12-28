21d41daa8c6a7a8a89a1aa4f9cb5950bfab5c041
PragmaTwice <i@twice.moe>
2019-12-28 14:26:44 +0800

    unit testing: test type checker by 'example/hello.typed.ichigo'

--- src/tests/type_checker.rs
+++ src/tests/type_checker.rs
@@ -1,122 +1,12 @@
 use crate::check::type_checker::*;
 use crate::syntax::ast::*;
+use crate::syntax::parser;
 
 #[test]
 fn test_example() {
-    let (checker, _) = TypeChecker::from_check(vec![
-        Bind::Type(
-            Ident("ℕ".to_string()),
-            Box::new(Type::Sum(vec![
-                Instance(
-                    Ident("0".to_string()),
-                    Box::new(Type::Var(Ident("ℕ".to_string()))),
-                ),
-                Instance(
-                    Ident("1+".to_string()),
-                    Box::new(Type::Map(
-                        Box::new(Type::Var(Ident("ℕ".to_string()))),
-                        Box::new(Type::Var(Ident("ℕ".to_string()))),
-                    )),
-                ),
-            ])),
-        ),
-        Bind::Expr(
-            Ident("+".to_string()),
-            Box::new(Expr::Lambda(vec![Pattern {
-                param: Box::new(Expr::Typed(
-                    Box::new(Expr::Var(Ident("x".to_string()))),
-                    Box::new(Type::Var(Ident("ℕ".to_string()))),
-                )),
-                expr: Box::new(Expr::Lambda(vec![
-                    Pattern {
-                        param: Box::new(Expr::Apply(
-                            Box::new(Expr::Var(Ident("1+".to_string()))),
-                            Box::new(Expr::Var(Ident("y".to_string()))),
-                        )),
-                        expr: Box::new(Expr::Apply(
-                            Box::new(Expr::Var(Ident("1+".to_string()))),
-                            Box::new(Expr::Apply(
-                                Box::new(Expr::Apply(
-                                    Box::new(Expr::Var(Ident("+".to_string()))),
-                                    Box::new(Expr::Var(Ident("x".to_string()))),
-                                )),
-                                Box::new(Expr::Var(Ident("y".to_string()))),
-                            )),
-                        )),
-                    },
-                    Pattern {
-                        param: Box::new(Expr::Var(Ident("0".to_string()))),
-                        expr: Box::new(Expr::Var(Ident("x".to_string()))),
-                    },
-                ])),
-            }])),
-        ),
-        Bind::Type(
-            Ident("ℕ𝓁".to_string()),
-            Box::new(Type::Sum(vec![
-                Instance(
-                    Ident("∅".to_string()),
-                    Box::new(Type::Var(Ident("ℕ𝓁".to_string()))),
-                ),
-                Instance(
-                    Ident("++".to_string()),
-                    Box::new(Type::Map(
-                        Box::new(Type::Var(Ident("ℕ".to_string()))),
-                        Box::new(Type::Map(
-                            Box::new(Type::Var(Ident("ℕ𝓁".to_string()))),
-                            Box::new(Type::Var(Ident("ℕ𝓁".to_string()))),
-                        )),
-                    )),
-                ),
-            ])),
-        ),
-        Bind::Expr(
-            Ident("take".to_string()),
-            Box::new(Expr::Lambda(vec![
-                Pattern {
-                    param: Box::new(Expr::Apply(
-                        Box::new(Expr::Var(Ident("1+".to_string()))),
-                        Box::new(Expr::Var(Ident("n".to_string()))),
-                    )),
-                    expr: Box::new(Expr::Lambda(vec![
-                        Pattern {
-                            param: Box::new(Expr::Var(Ident("∅".to_string()))),
-                            expr: Box::new(Expr::Var(Ident("∅".to_string()))),
-                        },
-                        Pattern {
-                            param: Box::new(Expr::Apply(
-                                Box::new(Expr::Apply(
-                                    Box::new(Expr::Var(Ident("++".to_string()))),
-                                    Box::new(Expr::Var(Ident("x".to_string()))),
-                                )),
-                                Box::new(Expr::Var(Ident("xs".to_string()))),
-                            )),
-                            expr: Box::new(Expr::Apply(
-                                Box::new(Expr::Apply(
-                                    Box::new(Expr::Var(Ident("++".to_string()))),
-                                    Box::new(Expr::Var(Ident("x".to_string()))),
-                                )),
-                                Box::new(Expr::Apply(
-                                    Box::new(Expr::Apply(
-                                        Box::new(Expr::Var(Ident("take".to_string()))),
-                                        Box::new(Expr::Var(Ident("n".to_string()))),
-                                    )),
-                                    Box::new(Expr::Var(Ident("xs".to_string()))),
-                                )),
-                            )),
-                        },
-                    ])),
-                },
-                Pattern {
-                    param: Box::new(Expr::Var(Ident("0".to_string()))),
-                    expr: Box::new(Expr::Lambda(vec![Pattern {
-                        param: Box::new(Expr::Var(Ident("x".to_string()))),
-                        expr: Box::new(Expr::Var(Ident("∅".to_string()))),
-                    }])),
-                },
-            ])),
-        ),
-    ]);
+    let (checker, typed_ast) = TypeChecker::from_check(parser::parse_file("example/hello.ichigo").unwrap());
+
+    assert_eq!(typed_ast.unwrap(), parser::parse_file("example/hello.typed.ichigo").unwrap());
 
     assert_eq!(
         checker.symbols,
